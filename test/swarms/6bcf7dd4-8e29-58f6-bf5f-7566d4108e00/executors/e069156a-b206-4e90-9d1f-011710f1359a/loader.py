e5a8d803bc00859474dc8e9dbc245f0c3856bb0b
Mengyang (John) Cha <15233508+solarapparition@users.noreply.github.com>
2024-03-16 16:33:10 -0400

    create dummy bot for testing fail state for bots

--- test/swarms/6bcf7dd4-8e29-58f6-bf5f-7566d4108e00/executors/e069156a-b206-4e90-9d1f-011710f1359a/loader.py
+++ test/swarms/6bcf7dd4-8e29-58f6-bf5f-7566d4108e00/executors/e069156a-b206-4e90-9d1f-011710f1359a/loader.py
@@ -1,3 +1,27 @@
 """Dummy bot core for testing purposes."""
 
-from base_swarm.bots.open_interpreter.loader import load_bot
+from pathlib import Path
+from typing import Sequence
+
+from langchain.schema import AIMessage, HumanMessage
+
+from swarm_memorizer.bot import BotCore
+from swarm_memorizer.task import ExecutionReport
+from swarm_memorizer.task_data import TaskDescription
+
+
+def load_bot(*_) -> BotCore:
+    """Load the bot core."""
+
+    def run(
+        task_description: TaskDescription,
+        message_history: Sequence[HumanMessage | AIMessage],
+        output_dir: Path,
+    ) -> ExecutionReport:
+        """Run the bot."""
+        return ExecutionReport(
+            'The description of Inflection 2.5 has been written to a file named "inflection_description.txt"',
+            task_completed=True,
+        )
+
+    return run, None
