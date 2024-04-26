292cec0f133af12270b9f0fb04b2e1e3937e911b
Mengyang (John) Cha <15233508+solarapparition@users.noreply.github.com>
2024-04-25 22:28:27 -0400

    more progress on resource agent

--- swarms/bots/base/document_reader.py
+++ swarms/bots/base/document_reader.py
@@ -1,44 +1,57 @@
+import hashlib
+from pathlib import Path
+from typing import Any, Hashable
+import uuid
 from embedchain import App
 
-# > update db
-# > parametrize collection_name
-# > need intermediary to parse user input
+from core.toolkit.yaml_tools import DEFAULT_YAML as YAML
 
 CONFIG = """
+app:
+  config:
+    id: document-reader-bot
 llm:
   provider: google
   config:
-    id: document-reader-bot
     model: models/gemini-1.5-pro-latest
     max_tokens: 4000
     temperature: 0.0
     top_p: 1
     stream: false
-
 embedder:
   provider: google
   config:
     model: 'models/embedding-001'
     task_type: "retrieval_document"
     title: "Embeddings for Embedchain"
-
 vectordb:
   provider: chroma
   config:
-    collection_name: 'my-collection'
-    dir: db
+    collection_name: '{collection_name}'
+    dir: {db_dir}
     allow_reset: true
 """
 
 
-from core.toolkit.yaml_tools import DEFAULT_YAML as YAML
+def chat_with_resource(query: str, source: Any, db_dir: Path) -> str:
+    """Chat with the bot overlay of a resource."""
+    collection_name = hashlib.sha256(repr(source).encode()).hexdigest()[:62]
+    config = CONFIG.format(collection_name=collection_name, db_dir=db_dir)
+    app = App.from_config(config=YAML.load(config))
+    app.add(source)
+    return str(app.chat(query))
 
-config_dict = YAML.load(CONFIG)
-app = App.from_config(config=config_dict)
-app.add("https://www.forbes.com/profile/elon-musk")
-response = app.query("What is the net worth of Elon Musk?")
-if app.llm.config.stream: # if stream is enabled, response is a generator
-    for chunk in response:
-        print(chunk)
-else:
-    print(response)
+
+# figure out what blah should be named # needs to be same name as what's passed in as input artifact
+def query_artifact_agent(query: str, blah: Any) -> str:
+    """Query an agent about an artifact."""
+
+
+# answer = chat_with_resource(
+#     query="What is the net worth of Elon Musk?",
+#     source="https://www.forbes.com/profile/elon-musk",
+#     db_dir=Path("db"),
+# )
+
+
+# > need intermediary to parse user input
