10cecd515b3b90aa160428cd8ccee46c5d64f356
Mengyang (John) Cha <15233508+solarapparition@users.noreply.github.com>
2024-05-03 10:06:14 -0400

    move rest of executors

---
+++ swarms/data/executors/08fbd09d-a63c-4067-bf02-375885317ed1/loader.py
@@ -0,0 +1,3 @@
+"""Bot for writing simple Python functions based on user requirements."""
+
+from swarms.bots.base.perplexity import load_bot
---
+++ swarms/data/executors/e069156a-b206-4e90-9d1f-011710f1359a/loader.py
@@ -0,0 +1,3 @@
+"""Bot for writing simple Python functions based on user requirements."""
+
+from swarms.bots.base.core_executor import load_bot
