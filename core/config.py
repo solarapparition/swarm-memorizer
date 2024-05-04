70495b48f4b2a282f24215c49231a4bf6f2f214c
Mengyang (John) Cha <15233508+solarapparition@users.noreply.github.com>
2024-05-04 11:25:21 -0400

    parametrize caching config function

--- core/config.py
+++ core/config.py
@@ -19,10 +19,11 @@ GPT_4_TURBO = "gpt-4-turbo-2024-04-09"
 AUTOGEN_CONFIG_LIST = [{"model": GPT_4_TURBO, "api_key": os.getenv("OPENAI_API_KEY")}]
 
 
-def configure_langchain_cache() -> None:
+def configure_langchain_cache(
+    database_path: Path = CACHE_DIR / ".langchain.db",
+) -> None:
     """Configure the LLM cache."""
-    makedirs(CACHE_DIR, exist_ok=True)
+    # makedirs(CACHE_DIR, exist_ok=True)
+    makedirs(database_path.parent, exist_ok=True)
     if not langchain.globals.get_llm_cache():
-        langchain.globals.set_llm_cache(
-            SQLiteCache(database_path=str(CACHE_DIR / ".langchain.db"))
-        )
+        langchain.globals.set_llm_cache(SQLiteCache(database_path=str(database_path)))
